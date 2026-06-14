6a4060fa88f03baf9f678a89559122acd899b828
Aamershaikh09 <ey0955015@gmail.com>
2026-06-14 12:20:42 +0530

    added some python programs

---
+++ practice1.py
@@ -0,0 +1,48 @@
+#write a program to display a person's name age and address in three different line
+name="Amer"
+age=21
+Address="Mumbai"
+print(name)
+print(age)
+print(Address)
+
+
+# swap two variables
+a=10
+b=15
+temp=a
+a=b
+b=temp
+print(a)
+print(b)
+
+
+#write a program to convert float into integer
+num1=10.23
+print(type(num1))
+x=int(num1)
+print(type(x))
+ 
+
+
+#write a program to take details from a student for id card then print it in different line
+name=input("Enter your name: ")
+age=input("Enter your age: ")
+Gender=input("Enter your gender: ")
+grade=input("Enter your grade: ")
+print("Student id card")
+print(name)
+print(age)
+print(Gender)
+print(grade)
+
+
+
+#write a program to take user input as integer and convert it into float
+num=int(input("Enter a number: "))
+print(num)
+print(type(num))
+print("After conversion")
+z=float(num)
+print(z)
+print(type(z))

ed435b28840a0a5b3377ea4e02163f3e3ac6dd4d
Aamershaikh09 <ey0955015@gmail.com>
2026-07-17 21:31:28 +0530

    recall

---
+++ practice11.py
@@ -0,0 +1,73 @@
+# Recall
+#Write a program to print "Hello, World!" on the screen.
+print("hello world!")
+
+#Write a program to read two numbers and print their sum
+a=10
+b=20
+c=a+b
+print(c)
+
+#Write a program to read two numbers and print their sum, difference, product and quotient.
+a=10
+b=50
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+print(c,d,e,f)
+
+#Write a program to read the radius of a circle and print its area and circumference
+radius=5
+r=radius*radius
+area_of_circle=3.14*r
+circu_of_circle=2*3.14*radius
+print(area_of_circle)
+print(circu_of_circle)
+
+#Write a program to read the length and breadth of a rectangle and print its area and perimeter.
+length=10
+breadth=20
+area_of_rec=length*breadth
+print(area_of_rec)
+peri_of_rec=2*length+2*breadth
+print(peri_of_rec)
+
+#Write a program to swap two numbers using a third variable
+a=10
+b=20
+c=a
+a=b
+b=c
+print(a,b)
+
+#Write a program to swap two numbers without using a third variable
+a=10
+b=1
+a=a+b
+b=a-b
+a=a-b
+print(a,b)
+
+#Write a program to read a temperature in Celsius and convert it to Fahrenheit
+celsius=100
+x=9/5
+fahrnheit=((celsius*x)+32)
+print(fahrnheit)
+
+#Write a program to read the marks of 5 subjects and print the total and average
+maths=60
+sci=70
+bio=59
+lang=72
+comp=66
+avg=maths+sci+bio+lang+comp/5
+print(avg)
+
+#Write a program to read seconds and convert them into hours, minutes and seconds
+sec=360
+hr=sec/3600
+min=sec/60
+print(sec,"in seconds")
+print(hr,"in hours")
+print(min,"in minutes")

15a2e9c4f87eabbb143bb5e8821210777d2b88b8
Aamershaikh09 <ey0955015@gmail.com>
2026-07-22 22:46:52 +0530

    recall(if/else)

---
+++ practice12.py
@@ -0,0 +1,41 @@
+#Practice(Conditional statements)
+#1 Write a program to read a number and check whether it is even or odd
+# num=int(input("enter a number :"))
+# if (num%2==0):
+#     print("It's an even number")
+# else:
+#     print("It's an odd")
+
+#2 Write a program to read a number and check whether it is positive, negative or zero.
+# num1=int(input("Enter a number: "))
+# if(num1>0):
+#     print("it's postive")
+# elif(num1<0):
+#     print("It's negative number")
+# else:
+#     print("It's zero")
+
+#3.Write a program to read three numbers and find the largest among them.
+# a=1000
+# b=9000
+# c=100
+
+# if(a>b) and(a>c):
+#     print(a,"is the largest")
+# elif(b>a)and (b>c):
+#     print(b,"is the largest")
+# else:
+#     print(c , "is the largest")
+
+# 4 Write a program to read three numbers and find the smallest among 
+# a=900
+# b=90
+# c=100
+
+# if(a<b) and(a<c):
+#     print(a,"is the smallest")
+# elif(b<a)and (b<c):
+#     print(b,"is the smallest")
+# else:
+#     print(c , "is the smallest")
+

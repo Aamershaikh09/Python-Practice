776bf203a6291330ce77924021c6e88f1b9167a0
Aamershaikh09 <ey0955015@gmail.com>
2026-08-09 22:44:47 +0530

    complete python in 3 days (day1)

---
+++ Complete python.py
@@ -0,0 +1,51 @@
+# complete python
+print("Hello world")
+# practice set 1
+#Q1 print Twinkle twnikle littel star 
+# print('''
+#  Twinkle, twinkle, little star,
+# How I wonder what you are!
+# Up above the world so high,
+# Like a diamond in the sky.
+
+# When the blazing sun is gone,
+# When he nothing shines upon,
+# Then you show your little light,
+# Twinkle, twinkle, all the night.
+
+# Then the traveler in the dark
+# Thanks you for your tiny spark,
+# How could he see where to go,
+# If you did not twinkle so?
+
+# In the dark blue sky you keep,
+# Often through my curtains peep
+# For you never shut your eye,
+# Till the sun is in the sky.
+
+# As your bright and tiny spark
+# Lights the traveler in the dark,
+# Though I know not what you are,
+# Twinkle, twinkle, little star.
+
+# ''')
+
+#Q2 install a external modlue and perform a program
+# import pyttsx3 
+# engine = pyttsx3.init()
+
+
+# engine.say(" Mera naam aamer")
+# engine.runAndWait()
+
+#q3 write a program to print content of dictionary using osmodule,serach online a function that does that
+# import os
+# #Specify the dictionary which i want to list
+# dire_path="/"
+# #list all files and dictioneries in the specific path
+# contents=os.listdir(dire_path)
+# #print each file and directory name
+# for i in contents:
+#     print(i)
+
+#Chapter 2 – Variables and Datatype
